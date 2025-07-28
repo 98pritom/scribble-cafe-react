d14196235bdb4552e7dfee1feb1f4f44e18fc670
Pritom <98pritom@gmail.com>
2025-07-28 22:22:09 +0600

    bookmarks added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import profile from '../../assets/images/profile.png';
 
 const Header = () => {
     return (
-        <>
-            <h1 className='text-4xl font-bold'>Scribble Cafe</h1>
-        </>
+        <header>
+            <div className='flex justify-between items-center p-4 mx-4 border-gray-200 border-b-2'>
+                <h1 className='text-4xl font-bold'>Scribble Cafe</h1>
+                <img src={profile} alt="" />
+            </div>
+        </header>
     );
 };
 
